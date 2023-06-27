bc8ea047cc851786d3e65ad9081593e3a041bfaf
User_JfInfrator <131720253+User-JfInfrator@users.noreply.github.com>
2023-06-27 19:21:31 -0300

    DadosMedico.java

--- src/main/java/br/com/tech4me/clinica/Model/DadosMedico.java
+++ src/main/java/br/com/tech4me/clinica/Model/DadosMedico.java
@@ -1,5 +1,27 @@
 package br.com.tech4me.clinica.Model;
 
-public record DadosMedico(String nome, String especialidade, double valorConsulta) {
+public class DadosMedico {
+    private String nome;
+    private String especialidade;
+    private double valorConsulta;
+
     
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+    public double getValorConsulta() {
+        return valorConsulta;
+    }
+    public void setValorConsulta(double valorConsulta) {
+        this.valorConsulta = valorConsulta;
+    }
 }
